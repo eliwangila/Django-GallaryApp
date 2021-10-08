7fe1c64a9df4b091c80f003b701684af19ad3c3f
eliwangila <eli.wangila@student.moringaschool.com>
2021-10-08 16:44:03 +0300

    add search image query fuction

--- photos/views.py
+++ photos/views.py
@@ -16,18 +16,18 @@ def main(request):
     except ObjectDoesNotExist:
         raise Http404()
     return render(request,'index.html',{'images':images,'category':category, 'location':location})
-# def search_images(request):
-#     if 'category' in request.GET and request.GET["category"]:
-#         search_term = request.GET.get("category")
-#         searched_category = Image.search_by_category(search_term)
-#         message = f"{search_term}"
-#         print(searched_category)
+def search_images(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_category = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        print(searched_category)
 
-#         return render(request, 'search.html',{"message":message,"image": searched_category})
+        return render(request, 'search.html',{"message":message,"image": searched_category})
 
-#     else:
-#         message = "You haven't searched for any term"
-#         return render(request, 'search.html',{'message':message})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{'message':message})
     
 # def view_by_location(request,location):
 #     try:
