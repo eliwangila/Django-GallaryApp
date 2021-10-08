0c4b8b2f8f61e98a7ddeea0e0d9386d7910e3bfa
eliwangila <eli.wangila@student.moringaschool.com>
2021-10-08 11:37:26 +0300

    add class location & category

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+    
+# class Image(models.Model):
+#     image = CloudinaryField('image')
+#     image_name = models.CharField(max_length=40)
+#     description = models.TextField()
+#     date = models.DateTimeField(auto_now_add=True)
+#     location = models.ForeignKey(Location, on_delete=models.CASCADE,default='')
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE,default='')
+    
--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Location,Category,Image
 
 # Create your views here.
 def welcome(request):
