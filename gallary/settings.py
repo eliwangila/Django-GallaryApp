45691a895533f3ad4814285db7eb636b123bee80
eliwangila <eli.wangila@student.moringaschool.com>
2021-10-08 12:07:24 +0300

    add database & bootsrap module

--- gallary/settings.py
+++ gallary/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'photos',
+    'boostrap5'
     'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -77,10 +78,13 @@ WSGI_APPLICATION = 'gallary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photos',
+        'USER': 'ekirapa',
+    'PASSWORD':'99405897',
     }
 }
+
 
 
 # Password validation
